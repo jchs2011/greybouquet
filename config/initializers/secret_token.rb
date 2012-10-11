a3947304fd1dfc043392ad87e9686e1eb9071a01
K. Stewart <ignitiant@gmail.com>
2012-10-10 20:41:32 -0500

    Initial commit: no extra junk

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greybouquet::Application.config.secret_token = '972f2dfab1b435077305f85de7376d563004bcf1dca888c3d5bf9dbddb05bbcf4427d105aaf9d304295be47019d903e826ca28f10462351c49cc27137e2300de'
