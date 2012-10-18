b0a406e183646c957b35ec4505780aba7fa2e5fd
K. Stewart <ignitiant@gmail.com>
2012-10-17 21:00:07 -0500

    Added images to comics, casts #index, _form

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,5 +1,5 @@
 class Comic < ActiveRecord::Base
-  attr_accessible :series, :strip
+  attr_accessible :series, :strip, :sequence
   has_attached_file :strip, :styles => { :medium => "300x300>", :thumb => "100x100>" },
       :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
       :url => "/system/:attachment/:id/:style/:filename"
